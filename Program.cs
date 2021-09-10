427c38b2ba5541624a7eced99e9447e815e717bb
Radostnyy <dmitro.laukhin@gmail.com>
2021-09-10 09:30:19 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace Lab1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine("Hello man!");
             Console.ReadLine();
         }
     }
